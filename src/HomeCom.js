6f6065fc165c362be9591dd7571a3dc8f75039d9
chris-jburke <cjburke04@gmail.com>
2020-04-17 14:31:36 -0700

    finished

--- src/HomeCom.js
+++ src/HomeCom.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Box, Button} from '@material-ui/core'
 import {Redirect, Link as RouterLink } from 'react-router-dom'
-
+import LogoColorIcon from './LogoIcon'
 
 
 const HomeCom = (props) => {
@@ -11,7 +11,8 @@ const HomeCom = (props) => {
 	return (
 		<Box style={{"maxWidth":"100%","height":"80vh","display":"flex","justifyContent":"center","alignItems":"center", "alignContent":"center"}}>
 			<Box style={{"textAlign":"center"}}>
-				<h1>Closet Minimizer</h1>
+				<span><LogoColorIcon /></span>
+				<h1>Eco Closet</h1>
 				<Button variant="outlined" component={LinkBehavior}>Sign In</Button>
 			</Box>
 		</Box>
